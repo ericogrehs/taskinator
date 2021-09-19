a710320965bb0319b4cd70e5991b36caadd4806d
Erico Grehs <ericogrehs@gmail.com>
2021-09-19 18:40:29 -0300

    feat: adds tag list to the creation todo form

--- src/components/CreateTaskForm.js
+++ src/components/CreateTaskForm.js
@@ -1,16 +1,19 @@
 import { useForm } from 'react-hook-form'
 import { Input, Button, Box, Stack } from '@chakra-ui/react'
 
+import TagList from './TagList'
+
 import { createTask } from '../services/tasks'
 
 function CreateTaskForm() {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, control } = useForm()
 
   const onSubmit = async data => {
     try {
       await createTask({
         text: data.title,
         type: 'todo',
+        tags: data.tags,
       })
     } catch (error) {
       console.log(error)
@@ -18,9 +21,12 @@ function CreateTaskForm() {
   }
 
   return (
-    <Box as='form' onSubmit={handleSubmit(onSubmit)} my='32px' w='360px'>
+    <Box as='form' onSubmit={handleSubmit(onSubmit)} my='32px' w='400px'>
       <Stack spacing='16px'>
-        <Input placeholder='Nome da Tarefa' {...register('title')} />
+        <Stack direction='row'>
+          <Input placeholder='Nome da Tarefa' {...register('title')} />
+          <TagList control={control} name='tags' />
+        </Stack>
         <Button type='submit' colorScheme='blue'>
           Criar
         </Button>
