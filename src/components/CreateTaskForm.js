7e9879835fe95a33ec33bd9c8a218da91ea54e8a
Erico Grehs <ericogrehs@gmail.com>
2021-09-20 15:45:36 -0300

    feat: add duedate on form

--- src/components/CreateTaskForm.js
+++ src/components/CreateTaskForm.js
@@ -1,11 +1,13 @@
 import { useForm } from 'react-hook-form'
 import { Input, Button, Box, Stack } from '@chakra-ui/react'
+import InputMask from 'react-input-mask'
 
 import TagList from './TagList'
 
 import { createTask } from '../services/tasks'
 import useToastEmitter from '../hooks/useToastEmitter'
 import useStore from '../hooks/useStore'
+import { formatDateToApi } from '../helpers/formatters'
 
 function CreateTaskForm() {
   const { register, handleSubmit, control, setValue } = useForm()
@@ -19,6 +21,7 @@ function CreateTaskForm() {
         text: data?.title,
         type: 'todo',
         tags: data?.tags,
+        date: formatDateToApi(data?.duedate),
       })
       toast.success('Tarefa criada com sucesso')
       fetchTasks()
@@ -37,6 +40,13 @@ function CreateTaskForm() {
           <Input placeholder='Nome da Tarefa' {...register('title')} />
           <TagList control={control} name='tags' />
         </Stack>
+        <Input
+          as={InputMask}
+          mask='99/99/9999'
+          // maskChar={null}
+          placeholder='Data da Tarefa'
+          {...register('duedate')}
+        />
         <Button type='submit' colorScheme='blue'>
           Criar
         </Button>
