84ca32f445cc0675387ae7ea149667f0e309efb6
Erico Grehs <ericogrehs@gmail.com>
2021-09-25 15:01:29 -0300

    feat: complete task

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,7 +1,37 @@
+import { useState } from 'react'
 import { Checkbox } from '@chakra-ui/react'
 
+import { completeTask } from '../services/tasks'
+import useStore from '../hooks/useStore'
+import useToastEmitter from '../hooks/useToastEmitter'
+import { IDLE, SUBMITTING } from '../constants/states'
+
 const TaskItem = ({ task }) => {
-  return <Checkbox>{task.text}</Checkbox>
+  const [state, setState] = useState(IDLE)
+
+  const { fetchTasks } = useStore()
+  const toast = useToastEmitter()
+
+  const onCheck =
+    id =>
+    async ({ target: { checked } }) => {
+      try {
+        if (!checked) return
+        setState(SUBMITTING)
+        await completeTask(id)
+        await fetchTasks()
+      } catch (error) {
+        console.error(error?.response?.data)
+        toast.error('Ocorreu um erro ao tentar completar a tarefa')
+        setState(IDLE)
+      }
+    }
+
+  return (
+    <Checkbox isDisabled={state === SUBMITTING} onChange={onCheck(task.id)}>
+      {task.text}
+    </Checkbox>
+  )
 }
 
 export default TaskItem
--- src/services/tasks.js
+++ src/services/tasks.js
@@ -5,3 +5,6 @@ export const createTask = body =>
 
 export const getTodos = () =>
   habiticaClient.get('/api/v3/tasks/user?type=todos')
+
+export const completeTask = taskId =>
+  habiticaClient.post(`https://habitica.com/api/v3/tasks/${taskId}/score/up`)
